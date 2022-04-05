b4103c6df9cb39138f33549a5ffa8923ba19d3e4
Alexander Jung <alex@unikraft.io>
2022-04-05 15:26:10 +0200

    internal/ghrepo: Import GitHub CLI representation of a GitHub repo
    This commit directly imports a slightly modified package from GitHub's CLI
    tool and places it within the `internal/ghrepo` directory[0].  The copy is
    partially direct and makes necessary copyright attribution which is
    facilitated by placing the contents of the project's LICENSE (MIT) as a
    comment preamble along with SPX identifier.
    
    The primary modification to the package has been removing the dependency to
    GitHub CLI's internap `ghinstance` package which aids in distinguishing
    between GitHub's public API and an Enterprise "self-hosted" API.  For now,
    such a differentiation is not necessary.
    
    [0]: https://github.com/cli/cli/tree/1eefda0/internal/ghrepo
    
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/briandowns/spinner v1.18.1
+	github.com/cli/cli/v2 v2.7.0
 	github.com/cli/safeexec v1.0.0
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510
 	github.com/henvic/httpretty v0.0.6
@@ -11,18 +12,18 @@ require (
 	github.com/mattn/go-isatty v0.0.14
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d
 	github.com/muesli/reflow v0.3.0
-	github.com/muesli/termenv v0.11.0
+	github.com/muesli/termenv v0.11.1-0.20220204035834-5ac8409525e0
 	github.com/stretchr/testify v1.7.1
 	golang.org/x/sys v0.0.0-20220405052023-b1e9470b6e64
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.7.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ internal/ghrepo/repo.go
@@ -0,0 +1,274 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2019 GitHub Inc.
+//               2022 Unikraft UG.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package ghrepo
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/cli/cli/v2/git"
+)
+
+// Interface describes an object that represents a GitHub repository
+type Interface interface {
+	RepoName() string
+	RepoOwner() string
+	RepoHost() string
+}
+
+// New instantiates a GitHub repository from owner and name arguments
+func New(owner, repo string) Interface {
+	return NewWithHost(owner, repo, "github.com")
+}
+
+// NewWithHost is like New with an explicit host name
+func NewWithHost(owner, repo, hostname string) Interface {
+	return &ghRepo{
+		owner:    owner,
+		name:     repo,
+		hostname: normalizeHostname(hostname),
+	}
+}
+
+// FullName serializes a GitHub repository into an "OWNER/REPO" string
+func FullName(r Interface) string {
+	return fmt.Sprintf("%s/%s", r.RepoOwner(), r.RepoName())
+}
+
+var defaultHostOverride string
+
+func defaultHost() string {
+	if defaultHostOverride != "" {
+		return defaultHostOverride
+	}
+
+	return "github.com"
+}
+
+// SetDefaultHost overrides the default GitHub hostname for FromFullName.
+// TODO: remove after FromFullName approach is revisited
+func SetDefaultHost(host string) {
+	defaultHostOverride = host
+}
+
+// FromFullName extracts the GitHub repository information from the following
+// formats: "OWNER/REPO", "HOST/OWNER/REPO", and a full URL.
+func FromFullName(nwo string) (Interface, error) {
+	return FromFullNameWithHost(nwo, defaultHost())
+}
+
+// FromFullNameWithHost is like FromFullName that defaults to a specific host
+// for values that don't explicitly include a hostname.
+func FromFullNameWithHost(nwo, fallbackHost string) (Interface, error) {
+	if git.IsURL(nwo) {
+		u, err := git.ParseURL(nwo)
+		if err != nil {
+			return nil, err
+		}
+		return FromURL(u)
+	}
+
+	parts := strings.SplitN(nwo, "/", 4)
+	for _, p := range parts {
+		if len(p) == 0 {
+			return nil, fmt.Errorf(`expected the "[HOST/]OWNER/REPO" format, got %q`, nwo)
+		}
+	}
+	switch len(parts) {
+	case 3:
+		return NewWithHost(parts[1], parts[2], parts[0]), nil
+	case 2:
+		return NewWithHost(parts[0], parts[1], fallbackHost), nil
+	default:
+		return nil, fmt.Errorf(`expected the "[HOST/]OWNER/REPO" format, got %q`, nwo)
+	}
+}
+
+// FromURL extracts the GitHub repository information from a git remote URL
+func FromURL(u *url.URL) (Interface, error) {
+	if u.Hostname() == "" {
+		return nil, fmt.Errorf("no hostname detected")
+	}
+
+	parts := strings.SplitN(strings.Trim(u.Path, "/"), "/", 3)
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("invalid path: %s", u.Path)
+	}
+
+	return NewWithHost(parts[0], strings.TrimSuffix(parts[1], ".git"), u.Hostname()), nil
+}
+
+func normalizeHostname(h string) string {
+	return strings.ToLower(strings.TrimPrefix(h, "www."))
+}
+
+// IsSame compares two GitHub repositories
+func IsSame(a, b Interface) bool {
+	return strings.EqualFold(a.RepoOwner(), b.RepoOwner()) &&
+		strings.EqualFold(a.RepoName(), b.RepoName()) &&
+		normalizeHostname(a.RepoHost()) == normalizeHostname(b.RepoHost())
+}
+
+func GenerateRepoURL(repo Interface, p string, args ...interface{}) string {
+	baseURL := fmt.Sprintf(
+		"https://%s/%s/%s",
+		repo.RepoHost(),
+		repo.RepoOwner(),
+		repo.RepoName(),
+	)
+	if p != "" {
+		if path := fmt.Sprintf(p, args...); path != "" {
+			return baseURL + "/" + path
+		}
+	}
+	return baseURL
+}
+
+// TODO there is a parallel implementation for non-isolated commands
+func FormatRemoteURL(repo Interface, protocol string) string {
+	if protocol == "ssh" {
+		return fmt.Sprintf("git@%s:%s/%s.git", repo.RepoHost(), repo.RepoOwner(), repo.RepoName())
+	}
+
+	return fmt.Sprintf("https://%s%s/%s.git", repo.RepoHost(), repo.RepoOwner(), repo.RepoName())
+}
+
+type ReleaseAsset struct {
+	Name   string
+	APIURL string `json:"url"`
+}
+
+type Release struct {
+	Tag    string `json:"tag_name"`
+	Assets []ReleaseAsset
+}
+
+func HasScript(httpClient *http.Client, repo Interface) (hs bool, err error) {
+	url := fmt.Sprintf(
+		"https://api.%s/repos/%s/%s/contents/%s",
+		repo.RepoHost(),
+		repo.RepoOwner(),
+		repo.RepoName(),
+		repo.RepoName(),
+	)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 404 {
+		return
+	}
+
+	hs = true
+	return
+}
+
+// DownloadAsset downloads a single asset to the given file path.
+func DownloadAsset(httpClient *http.Client, asset ReleaseAsset, destPath string) error {
+	req, err := http.NewRequest("GET", asset.APIURL, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Accept", "application/octet-stream")
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	f, err := os.OpenFile(destPath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o755)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = io.Copy(f, resp.Body)
+	return err
+}
+
+// FetchLatestRelease finds the latest published release for a repository.
+func FetchLatestRelease(httpClient *http.Client, baseRepo Interface) (*Release, error) {
+	url := fmt.Sprintf(
+		"https://api.%s/repos/%s/%s/releases/latest",
+		baseRepo.RepoHost(),
+		baseRepo.RepoOwner(),
+		baseRepo.RepoName(),
+	)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var r Release
+	err = json.Unmarshal(b, &r)
+	if err != nil {
+		return nil, err
+	}
+
+	return &r, nil
+}
+
+type ghRepo struct {
+	owner    string
+	name     string
+	hostname string
+}
+
+func (r ghRepo) RepoOwner() string {
+	return r.owner
+}
+
+func (r ghRepo) RepoName() string {
+	return r.name
+}
+
+func (r ghRepo) RepoHost() string {
+	return r.hostname
+}
