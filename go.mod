961a8b04d8d8882534e7c8f273435621cda034ff
Alexander Jung <alex@unikraft.io>
2022-04-15 10:17:07 +0200

    internal/cmdfactory: Introduce `cmdfactory`
    The command factory is an internal utility package which provides a higher-
    order level of abstraction to new commands, allowing the command to access
    commonly exposed interfaces accross the repository.  For now, the
    implementation is minimal and simply includes directives such as access to
    common config, a HTTP client as well as knowing the working directory.
    
    The motive behind this package is to be able to introduce `With` option
    arguments to the factory during initialisation and to further extend the
    command's reach to different subsystems.  For example, a new command can
    instantiate itself with a command factory which has access to the plugin
    manager, or the manifest manager, etc.  Some commands may need access to
    these common subsystems, others may not.  The command factory can help
    standardise access to such features.
    
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d
 	github.com/muesli/reflow v0.3.0
 	github.com/muesli/termenv v0.11.1-0.20220204035834-5ac8409525e0
+	github.com/spf13/cobra v1.4.0
 	github.com/stretchr/testify v1.7.1
 	golang.org/x/sys v0.0.0-20220405052023-b1e9470b6e64
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211
@@ -22,8 +23,10 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.7.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 )
---
+++ internal/cmdfactory/cmdfactory.go
@@ -0,0 +1,139 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Authors: Alexander Jung <alex@unikraft.io>
+//
+// Copyright (c) 2022, Unikraft UG.  All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+package cmdfactory
+
+import (
+	"errors"
+	"net/http"
+	"os"
+
+	"github.com/spf13/cobra"
+	"go.unikraft.io/kit/internal/config"
+	"go.unikraft.io/kit/internal/httpclient"
+	"go.unikraft.io/kit/pkg/iostreams"
+)
+
+type FactoryOption func(*Factory)
+
+type Factory struct {
+	RootCmd   *cobra.Command
+	IOStreams *iostreams.IOStreams
+
+	HttpClient func() (*http.Client, error)
+	Config     func() (config.Config, error)
+}
+
+// New creates a new Factory object
+func New(opts ...FactoryOption) *Factory {
+	f := &Factory{
+		Config: configFunc(),
+	}
+
+	f.IOStreams = ioStreams(f)       // Depends on Config
+	f.HttpClient = httpClientFunc(f) // Depends on Config, IOStreams
+
+	// Loop through each option
+	for _, opt := range opts {
+		// Call the option giving the instantiated *Factory as the argument
+		opt(f)
+	}
+
+	// Force the terminal if desired
+	if spec := os.Getenv("KRAFTKIT_FORCE_TTY"); spec != "" {
+		f.IOStreams.ForceTerminal(spec)
+	}
+
+	// Enable running kraftkit from Windows File Explorer's address bar.  Without
+	// this, the user is told to stop and run from a terminal.  With this, a user
+	// can take any action directly from explorer.
+	if len(os.Args) > 1 && os.Args[1] != "" {
+		cobra.MousetrapHelpText = ""
+	}
+
+	return f
+}
+
+func configFunc() func() (config.Config, error) {
+	var cachedConfig config.Config
+	var configError error
+
+	return func() (config.Config, error) {
+		if cachedConfig != nil || configError != nil {
+			return cachedConfig, configError
+		}
+
+		cachedConfig, configError = config.ParseDefaultConfig()
+		if errors.Is(configError, os.ErrNotExist) {
+			cachedConfig = config.NewBlankConfig()
+			configError = nil
+		}
+
+		cachedConfig = config.InheritEnv(cachedConfig)
+		return cachedConfig, configError
+	}
+}
+
+func ioStreams(f *Factory) *iostreams.IOStreams {
+	io := iostreams.System()
+	cfg, err := f.Config()
+	if err != nil {
+		return io
+	}
+
+	if prompt, _ := cfg.GetOrDefault("prompt"); prompt == "disabled" {
+		io.SetNeverPrompt(true)
+	}
+
+	// Pager precedence
+	// 1. KRAFTKIT_PAGER
+	// 2. pager from config
+	// 3. PAGER
+	if kkPager, kkPagerExists := os.LookupEnv("KRAFTKIT_PAGER"); kkPagerExists {
+		io.SetPager(kkPager)
+	} else if pager, _ := cfg.Get("pager"); pager != "" {
+		io.SetPager(pager)
+	}
+
+	return io
+}
+
+func httpClientFunc(f *Factory) func() (*http.Client, error) {
+	return func() (*http.Client, error) {
+		io := f.IOStreams
+		cfg, err := f.Config()
+		if err != nil {
+			return nil, err
+		}
+
+		return httpclient.NewHTTPClient(io, cfg, true)
+	}
+}
