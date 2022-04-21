b3fdcb8bd3d4ea007d0bcb4feeed6cc559ab72da
Alexander Jung <alex@unikraft.io>
2022-04-21 22:11:54 +0200

    schema/loader: Introduce `LoaderOptions` for aiding to load a spec
    This commit imports a partial from the compose specification loader[0]
    which is useful for managing options for the loader program.  This program
    is intended to read and parse a YAML specification file.  This commit also
    makes necessary copyright attribution which is facilitated by placing the
    contents of the project's LICENSE (Apache-2.0) as a comment preamble along
    with SPDX identifier.
    
    [0]: https://github.com/compose-spec/compose-go/blob/32e1258/loader/loader.go#L46-L79=
    
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/briandowns/spinner v1.18.1
 	github.com/cli/cli/v2 v2.7.0
 	github.com/cli/safeexec v1.0.0
+	github.com/compose-spec/compose-go v1.2.4
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510
 	github.com/henvic/httpretty v0.0.6
 	github.com/mattn/go-colorable v0.1.12
@@ -30,8 +31,10 @@ require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	golang.org/x/text v0.3.7 // indirect
---
+++ schema/loader/options.go
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright 2020 The Compose Specification Authors.
+// Copyright 2022 Unikraft UG. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+// 		http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package loader
+
+import (
+	interp "github.com/compose-spec/compose-go/interpolation"
+)
+
+// LoaderOptions supported by Load
+type LoaderOptions struct {
+	// Skip schema validation
+	SkipValidation bool
+	// Skip interpolation
+	SkipInterpolation bool
+	// Skip normalization
+	SkipNormalization bool
+	// Resolve paths
+	ResolvePaths bool
+	// Interpolation options
+	Interpolate *interp.Options
+	// Set project projectName
+	projectName string
+	// Indicates when the projectName was imperatively set or guessed from path
+	projectNameImperativelySet bool
+}
+
+func (o *LoaderOptions) SetProjectName(name string, imperativelySet bool) {
+	o.projectName = normalizeProjectName(name)
+	o.projectNameImperativelySet = imperativelySet
+}
+
+func (o LoaderOptions) GetProjectName() (string, bool) {
+	return o.projectName, o.projectNameImperativelySet
+}
+
+// WithSkipValidation sets the LoaderOptions to skip validation when loading
+// sections
+func WithSkipValidation(opts *LoaderOptions) {
+	opts.SkipValidation = true
+}
