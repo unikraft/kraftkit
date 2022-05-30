0c9d23498154518508b4c4f5e82f19af74266998
Alexander Jung <alex@unikraft.io>
2022-05-30 19:45:27 +0200

    internal/logger,pkg/log: Introduce an abstract interface for the logger
    Golang currently does not export a generic logging interface, meaning that
    potential users who wish to use KraftKit's exposed modules inside of pkg/
    are completely reliant on the internal logging system.  This may not suite
    the context of the importing program.  In this commit, we introduce an
    abstract and exported `Logger` interface within `pkg/log` such that we can
    pass in other loggers and logging systems which adopt these methods.
    
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -58,11 +58,22 @@ type Logger struct {
 	ExitOnFatal bool
 }
 
-func (l *Logger) now() string {
+func (l Logger) now() string {
 	return time.Now().Format("2006-01-02 15:04:05")
 }
 
-func (l *Logger) log(level string, format string, a ...interface{}) {
+func (l Logger) log(level string, a ...interface{}) {
+	magenta := l.io.ColorScheme().SprintFunc("magenta")
+
+	fmt.Fprintf(l.io.Out, "%s %s %s %s\n",
+		magenta(l.now()),
+		level,
+		magenta(":"),
+		fmt.Sprint(a...),
+	)
+}
+
+func (l Logger) logf(level string, format string, a ...interface{}) {
 	magenta := l.io.ColorScheme().SprintFunc("magenta")
 
 	fmt.Fprintf(l.io.Out, "%s %s %s %s\n",
@@ -74,42 +85,80 @@ func (l *Logger) log(level string, format string, a ...interface{}) {
 }
 
 // SetLevel updates the logging level for future logs
-func (l *Logger) SetLevel(level LogLevel) {
+func (l Logger) SetLevel(level LogLevel) {
 	l.level = level
 }
 
-func (l *Logger) Trace(format string, a ...interface{}) {
+func (l Logger) Trace(a ...interface{}) {
 	if l.level == TRACE {
-		l.log(l.trace("TRACE"), format, a...)
+		l.log(l.trace("TRACE"), a...)
 	}
 }
 
-func (l *Logger) Debug(format string, a ...interface{}) {
-	if l.level <= DEBUG {
-		l.log(l.debug("DEBUG"), format, a...)
+func (l Logger) Tracef(format string, a ...interface{}) {
+	if l.level == TRACE {
+		l.logf(l.trace("TRACE"), format, a...)
 	}
 }
 
-func (l *Logger) Info(format string, a ...interface{}) {
-	if l.level <= INFO {
-		l.log(l.info(" INFO"), format, a...)
+func (l Logger) Debug(a ...interface{}) {
+	if l.level >= DEBUG {
+		l.log(l.debug("DEBUG"), a...)
 	}
 }
 
-func (l *Logger) Warn(format string, a ...interface{}) {
-	if l.level <= WARN {
-		l.log(l.warn(" WARN"), format, a...)
+func (l Logger) Debugf(format string, a ...interface{}) {
+	if l.level >= DEBUG {
+		l.logf(l.debug("DEBUG"), format, a...)
 	}
 }
 
-func (l *Logger) Error(format string, a ...interface{}) {
-	if l.level <= ERROR {
-		l.log(l.err("ERROR"), format, a...)
+func (l Logger) Info(a ...interface{}) {
+	if l.level >= INFO {
+		l.log(l.info(" INFO"), a...)
 	}
 }
 
-func (l *Logger) Fatal(format string, a ...interface{}) {
-	l.log(l.fatal("FATAL"), format, a...)
+func (l Logger) Infof(format string, a ...interface{}) {
+	if l.level >= INFO {
+		l.logf(l.info(" INFO"), format, a...)
+	}
+}
+
+func (l Logger) Warn(a ...interface{}) {
+	if l.level >= WARN {
+		l.log(l.warn(" WARN"), a...)
+	}
+}
+
+func (l Logger) Warnf(format string, a ...interface{}) {
+	if l.level >= WARN {
+		l.logf(l.warn(" WARN"), format, a...)
+	}
+}
+
+func (l Logger) Error(a ...interface{}) {
+	if l.level >= ERROR {
+		l.log(l.err("ERROR"), a...)
+	}
+}
+
+func (l Logger) Errorf(format string, a ...interface{}) {
+	if l.level >= ERROR {
+		l.logf(l.err("ERROR"), format, a...)
+	}
+}
+
+func (l Logger) Fatal(a ...interface{}) {
+	l.log(l.fatal("FATAL"), a...)
+
+	if l.ExitOnFatal {
+		exit(1)
+	}
+}
+
+func (l Logger) Fatalf(format string, a ...interface{}) {
+	l.logf(l.fatal("FATAL"), format, a...)
 
 	if l.ExitOnFatal {
 		exit(1)
@@ -118,10 +167,10 @@ func (l *Logger) Fatal(format string, a ...interface{}) {
 
 // NewLogger creates a new logger
 // Default level is INFO
-func NewLogger(io *iostreams.IOStreams) *Logger {
+func NewLogger(io *iostreams.IOStreams) Logger {
 	cs := io.ColorScheme()
 
-	return &Logger{
+	return Logger{
 		io:          io,
 		level:       INFO,
 		trace:       cs.SprintFunc("blue"),
