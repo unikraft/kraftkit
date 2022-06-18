a05a2dc66a6d60331738e189f587790934080b2a
Alexander Jung <alex@unikraft.io>
2022-06-18 09:37:34 +0200

    pkg/manifest: Introduce `Provider` interface with `NewProvider`
    The `Provider` interface represents a mechanism within the `manifest`
    package which will ultimatelly allow the `ManifestManager` to be able to
    retrieve information resulting in a populated `Manifest` structure.  Since
    the `ManifestManager` can accept as its input source either a Git
    repository (in parity to the way in which the current version of the
    `kraft` operates) as well as the new YAML-defined meta files whose
    structure is defined in `Manifest` and `ManifestIndex`, the `NewProvider`
    method (and subsequent implementing providers) will be able to accept as
    input a `path` which can be tested against different providers.  A provider
    which understands the input `path` can then call the implementing
    interface's `Manifests()` method which returns a list of populated
    `Manifest` structures.
    
    Signed-off-by: Alexander Jung <alex@unikraft.io>

---
+++ pkg/manifest/provider.go
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Authors: Alexander Jung <alex@unikraft.io>
+//
+// Copyright (c) 2022, Unikraft UG.  All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+package manifest
+
+import "fmt"
+
+type Provider interface {
+	// Manifests returns a slice of Manifests which can be returned by this
+	// Provider
+	Manifests() ([]*Manifest, error)
+}
+
+// NewProvider ultimately returns one of the supported manifest providers by
+// attempting an ordered instantiation based on the input source.  For the
+// provider which does not return an error is indicator that it is supported and
+// thus the return of NewProvider a compatible interface Provider able to gather
+// information about the manifest.
+func NewProvider(path string, mopts ...ManifestOption) (Provider, error) {
+	return nil, fmt.Errorf("could not determine provider for: %s", path)
+}
