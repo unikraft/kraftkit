148110af01446e1e8b17dfc9b6307dd2c931d6eb
Alexander Jung <alex@unikraft.io>
2022-07-10 18:11:44 +0200

    pkg/manifest: Introduce `WithSourcesRootDir` option
    In this commit we introduce `WithSourcesRootDir` which helps signal to
    te Manifest the path to local cache of the resource.
    
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- pkg/manifest/options.go
+++ pkg/manifest/options.go
@@ -32,8 +32,11 @@
 package manifest
 
 import (
+	"path/filepath"
+
 	"go.unikraft.io/kit/config"
 	"go.unikraft.io/kit/pkg/log"
+	"go.unikraft.io/kit/pkg/unikraft"
 )
 
 type ManifestOption func(m *Manifest) error
@@ -48,6 +51,45 @@ func WithAuthConfig(auths map[string]config.AuthConfig) ManifestOption {
 func WithLogger(l log.Logger) ManifestOption {
 	return func(m *Manifest) error {
 		m.log = l
+		return nil
+	}
+}
+
+// WithSourcesRootDir is an option wich helps find cached Manifest Channel or
+// Version resources.  When set to a directory, the fixed structure of this
+// directory should allow us to look up (and also store) resources here for
+// later use.
+func WithSourcesRootDir(dir string) ManifestOption {
+	return func(m *Manifest) error {
+		// See: https://github.com/golang/go/wiki/CommonMistakes#using-reference-to-loop-iterator-variable
+		dir := dir
+
+		if m.Type != unikraft.ComponentTypeCore {
+			dir = filepath.Join(dir, m.Type.Plural())
+		}
+
+		for i, channel := range m.Channels {
+			ext := filepath.Ext(channel.Resource)
+			if ext == ".gz" {
+				ext = ".tar.gz"
+			}
+
+			m.Channels[i].Local = filepath.Join(
+				dir, m.Name + "-" + channel.Name + ext,
+			)
+		}
+
+		for i, version := range m.Versions {
+			ext := filepath.Ext(version.Resource)
+			if ext == ".gz" {
+				ext = ".tar.gz"
+			}
+
+			m.Versions[i].Local = filepath.Join(
+				dir, m.Name + "-" + version.Version + ext,
+			)
+		}
+
 		return nil
 	}
 }
