0d29d5bfbef5b79d0cfe8d40f662d9f7cdba2a90
Stefan Jumarea <stefanjumarea02@gmail.com>
2022-08-05 10:35:58 +0300

    cmd/kraft: Add ability to check for an update
    Add an automatic check for available updates on
    every kraft run.
    Add option to check for new prereleases.
    
    Signed-off-by: Stefan Jumarea <stefanjumarea02@gmail.com>

--- cmd/kraft/kraft.go
+++ cmd/kraft/kraft.go
@@ -32,8 +32,17 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
+	"time"
+	"strings"
+	"encoding/json"
+	"net/http"
 
+	"kraftkit.sh/config"
+
+	"github.com/spf13/cobra"
 	"github.com/MakeNowJust/heredoc"
 
 	"kraftkit.sh/internal/cmdfactory"
@@ -46,10 +55,39 @@ import (
 	"kraftkit.sh/cmd/kraft/rm"
 	"kraftkit.sh/cmd/kraft/run"
 	"kraftkit.sh/cmd/kraft/stop"
+	"kraftkit.sh/iostreams"
+	"kraftkit.sh/log"
+	"kraftkit.sh/packmanager"
 
 	// Additional initializers
 	_ "kraftkit.sh/manifest"
+	"kraftkit.sh/internal/version"
 )
+
+// The releases structure contains the relevant fields
+// from a github API release query response
+type releases []struct {
+	HtmlUrl string `json:"html_url"`
+	Id int `json:"id"`
+	IsPreRelease bool `json:"prerelease"`
+	Assets Assets `json:"assets"`
+	Name string `json:"name"`
+}
+
+type Assets []struct {
+	Name string `json:"name"`
+	Url string `json:"browser_download_url"`
+}
+
+type kraftOptions struct {
+	PackageManager func(opts ...packmanager.PackageManagerOption) (packmanager.PackageManager, error)
+	ConfigManager  func() (*config.ConfigManager, error)
+	Logger         func() (log.Logger, error)
+	IO             *iostreams.IOStreams
+
+	// Command line arguments
+	checkPreRelease		bool
+}
 
 func main() {
 	f := cmdfactory.New(
@@ -70,6 +108,13 @@ func main() {
 		panic("could not initialize root commmand")
 	}
 
+	opts := &kraftOptions{
+		PackageManager: f.PackageManager,
+		ConfigManager:  f.ConfigManager,
+		Logger:         f.Logger,
+		IO:             f.IOStreams,
+	}
+
 	cmd.Short = "Build and use highly customized and ultra-lightweight unikernels"
 	cmd.Long = heredoc.Docf(`
 
@@ -81,6 +126,146 @@ func main() {
    (_:| |:_)  Issues & support: https://github.com/unikraft/kraftkit/issues
       v v 
       ' '`)
+	cmd.RunE = func(cmd *cobra.Command, args []string) error {
+		return kraftRun(opts, f)
+	}
+
+	cmd.Flags().BoolVar(
+		&opts.checkPreRelease,
+		"check-prerelease",
+		false,
+		"Check for an available prerelease",
+	)
 
 	os.Exit(int(cmdutil.Execute(f, cmd)))
+}
+
+func kraftRun(opts *kraftOptions, cmdFactory *cmdfactory.Factory) error {
+	var err error
+
+	klog, err := opts.Logger()
+	if err != nil {
+		return err
+	}
+
+
+	// Check for a new prerelease
+	if opts.checkPreRelease {
+		updateAvail, err := checkForUpdate(cmdFactory, true)
+
+		if err != nil {
+			fmt.Errorf("Could not check for new prereleases: %s\n", err)
+		} else if updateAvail {
+			klog.Info("New version available!")
+		}
+	} else {
+
+		// If the check for prerelease option is not given, check for an update
+		updateAvail, err := checkForUpdate(cmdFactory, false)
+
+		if err != nil {
+			fmt.Errorf("Could not check for new releases: %s\n", err)
+		} else if updateAvail {
+			klog.Warn("New version available!")
+		}
+	}
+
+	return nil
+}
+
+// Check for an update, return true is an update is available
+// If the checkPrerelease param is true, check for a prerelease,
+// else check for a release
+func checkForUpdate(cmdFactory *cmdfactory.Factory, checkPrerelease bool) (bool, error) {
+	cfgm, err := cmdFactory.ConfigManager()
+	if err != nil {
+		return false, err
+	}
+
+	var token string
+	token = cfgm.Config.Auth["github"].Token
+
+	resp, err := makeReleasesReq(token)
+	if err != nil {
+		return false, err
+	}
+
+	var rel_arr releases
+	err = json.Unmarshal([]byte(resp), &rel_arr)
+	if err != nil {
+		return false, err
+	}
+
+	var index int
+	if checkPrerelease {
+		index = getLatestPreRelease(rel_arr)
+	} else {
+		index = getLatestRelease(rel_arr)
+	}
+
+	if index == -1 {
+		return false, nil
+	}
+
+	if !strings.Contains(version.Version(), rel_arr[index].Name) {
+		return true, nil
+	}
+	return false, nil
+}
+
+// Make a request to the github API to get an array of
+// all available releases for the kraftkit repo
+func makeReleasesReq(token string) (string, error) {
+	timeout := time.Duration(5 + time.Second)
+	client := http.Client {
+		Timeout: timeout,
+	}
+
+	req, err := http.NewRequest("GET",
+		"https://api.github.com/repos/unikraft/kraftkit/releases",
+		nil)
+
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("Accept", "application/vnd.github+json")
+
+	if token != "" {
+		req.Header.Set("Authorization", "token " + token)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
+
+// Get index of the first release in array
+func getLatestRelease(releaseArray releases) int {
+	for i := range releaseArray {
+		if !releaseArray[i].IsPreRelease {
+			return i
+		}
+	}
+
+	return -1
+}
+
+// Get index of the first prerelease in array
+func getLatestPreRelease(releaseArray releases) int {
+	for i := range releaseArray {
+		if releaseArray[i].IsPreRelease {
+			return i
+		}
+	}
+
+	return -1
 }
