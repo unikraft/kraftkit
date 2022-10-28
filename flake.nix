9ff93eea05533e0b18e5a734235fcb3573376f51
Marco Schlumpp <marco.schlumpp@gmail.com>
2022-10-28 15:19:13 +0200

    Remove arm and darwin from supported systems

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       # Generate a user-friendly version number.
       version = builtins.substring 0 8 lastModifiedDate;
 
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
     in
