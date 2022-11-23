2ba67cebf4a215f0905828435a4fe28dbbf7156e
Alexander Jung <alex@unikraft.io>
2022-11-23 14:48:34 +0100

    iostreams: Introduce contextual iostreams
    This commit introduces a new primitive to the `iostreams` package which
    enables access to a global iostreams via the use of the `context` package.
    Its use is exported via the `G` variable to be used inline via:
    
    ```go
    import (
    "context"
    "kraftkit.sh/iostreams"
    )
    
    iostreams.G(context.Background()).HasTrueColor()
    ```
    
    This will enable a refactor of the KraftKit codebase which will eventually forgo
    and remove all instances of `WithIOStreams` option methods or passing the
    `IOStreams` structure directly.  Their use enabled a constraint which prevented
    access to the iostream methods without access to a previously instantiated
    `IOStreams` structure.  In this mode, we only need to pass the richer `ctx`
    variable which, naturally, can be contextualised, to use a specific IO stream.
    
    Signed-off-by: Alexander Jung <alex@unikraft.io>

---
+++ iostreams/context.go
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022, Unikraft GmbH and The KraftKit Authors.
+// Licensed under the BSD-3-Clause License (the "License").
+// You may not use this file expect in compliance with the License.
+package iostreams
+
+import (
+	"context"
+)
+
+var (
+	// G is an alias for FromContext.
+	//
+	// We may want to define this locally to a package to get package tagged
+	// iostreams.
+	G = FromContext
+
+	// L is the system IO stream.
+	IO = System()
+)
+
+// contextKey is used to retrieve the logger from the context.
+type contextKey struct{}
+
+// WithIOStreams returns a new context with the provided logger. Use in
+// combination with logger.WithField(s) for great effect.
+func WithIOStreams(ctx context.Context, iostreams *IOStreams) context.Context {
+	return context.WithValue(ctx, contextKey{}, iostreams)
+}
+
+// FromContext returns the logger kraftkit in the context, or an inert logger
+// that will not log anything.
+func FromContext(ctx context.Context) *IOStreams {
+	l := ctx.Value(contextKey{})
+
+	if l == nil {
+		return IO
+	}
+
+	return l.(*IOStreams)
+}
