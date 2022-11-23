ab337d4d2abc54ec9639a7f0665c47e04c071fc1
Alexander Jung <alex@unikraft.io>
2022-11-23 13:07:10 +0100

    log: Introduce contextual logger
    This commit introduces a new primitive to the `log` package which
    enables access to a global logger via the use of the `context` package.
    Its use is exported via the `G` variable to be used inline via:
    
    ```go
    import (
      "context"
      "kraftkit.sh/log"
    )
    
    log.G(context.Background()).Info("example")
    ```
    
    This will enable a refactor of the KraftKit codebase which will
    eventually forgo and remove all instances of `WithLogger` option
    methods.  Their use enabled a constraint which prevented logging without
    access to a previously instantiated logger.  In this mode, we only need
    to pass the richer `ctx` variable which, naturally, can be
    contextualised, to use a specific logger.
    
    Signed-off-by: Alexander Jung <alex@unikraft.io>

---
+++ log/context.go
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022, Unikraft GmbH and The KraftKit Authors.
+// Licensed under the BSD-3-Clause License (the "License").
+// You may not use this file expect in compliance with the License.
+package log
+
+import (
+	"context"
+
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	// G is an alias for FromContext.
+	//
+	// We may want to define this locally to a package to get package tagged log
+	// messages.
+	G = FromContext
+
+	// L is the global logger.
+	L = logrus.NewEntry(logrus.StandardLogger())
+)
+
+// contextKey is used to retrieve the logger from the context.
+type contextKey struct{}
+
+// WithLogger returns a new context with the provided logger. Use in
+// combination with logger.WithField(s) for great effect.
+func WithLogger(ctx context.Context, logger *logrus.Entry) context.Context {
+	return context.WithValue(ctx, contextKey{}, logger)
+}
+
+// FromContext returns the logger kraftkit in the context, or an inert logger
+// that will not log anything.
+func FromContext(ctx context.Context) *logrus.Entry {
+	l, ok := ctx.Value(contextKey{}).(*logrus.Entry)
+	if !ok || l == nil {
+		return L
+	}
+
+	return l
+}
