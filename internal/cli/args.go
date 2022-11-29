2b97250dfd736f305574c36aecb213cc9bfa5e7a
Alexander Jung <alex@unikraft.io>
2022-11-29 12:50:12 +0100

    internal/cli: Migrate flag helper methods
    Signed-off-by: Alexander Jung <alex@unikraft.io>

---
+++ internal/cli/args.go
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2019, 2019 GitHub Inc.
+// Copyright (c) 2022, Unikraft GmbH and The KraftKit Authors.
+// Licensed under the MIT License (the "License").
+// You may not use this file expect in compliance with the License.
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
+
+func MinimumArgs(n int, msg string) cobra.PositionalArgs {
+	if msg == "" {
+		return cobra.MinimumNArgs(1)
+	}
+
+	return func(cmd *cobra.Command, args []string) error {
+		if len(args) < n {
+			return FlagErrorf("%s", msg)
+		}
+		return nil
+	}
+}
+
+func ExactArgs(n int, msg string) cobra.PositionalArgs {
+	return func(cmd *cobra.Command, args []string) error {
+		if len(args) > n {
+			return FlagErrorf("too many arguments")
+		}
+
+		if len(args) < n {
+			return FlagErrorf("%s", msg)
+		}
+
+		return nil
+	}
+}
+
+func NoArgsQuoteReminder(cmd *cobra.Command, args []string) error {
+	if len(args) < 1 {
+		return nil
+	}
+
+	errMsg := fmt.Sprintf("unknown argument %q", args[0])
+	if len(args) > 1 {
+		errMsg = fmt.Sprintf("unknown arguments %q", args)
+	}
+
+	hasValueFlag := false
+	cmd.Flags().Visit(func(f *pflag.Flag) {
+		if f.Value.Type() != "bool" {
+			hasValueFlag = true
+		}
+	})
+
+	if hasValueFlag {
+		errMsg += "; please quote all values that have spaces"
+	}
+
+	return FlagErrorf("%s", errMsg)
+}
+
+func MaxDirArgs(n int) cobra.PositionalArgs {
+	return func(cmd *cobra.Command, args []string) error {
+		if len(args) > n {
+			return FlagErrorf("expected no more than %d paths received %d", n, len(args))
+
+			// Treat no path as current working directory
+		} else if len(args) == 0 {
+			cwd, err := os.Getwd()
+			if err != nil {
+				return err
+			}
+
+			args = []string{cwd}
+		}
+
+		for _, path := range args {
+			f, err := os.Stat(path)
+			if err != nil || !f.IsDir() {
+				return FlagErrorf("path is not a valid directory: %s", path)
+			}
+		}
+
+		return nil
+	}
+}
---
+++ internal/cli/flags_bool.go
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2012 Alex Ogier.
+// Copyright (c) 2012 The Go Authors.
+// Copyright (c) 2022, Unikraft GmbH and The KraftKit Authors.
+// Licensed under the BSD-3-Clause License (the "License").
+// You may not use this file expect in compliance with the License.
+package cli
+
+import (
+	"strconv"
+
+	"github.com/spf13/pflag"
+)
+
+type boolValue bool
+
+func newBoolValue(val bool, p *bool) *boolValue {
+	*p = val
+	return (*boolValue)(p)
+}
+
+func (b *boolValue) Set(val string) error {
+	v, err := strconv.ParseBool(val)
+	*b = boolValue(v)
+	return err
+}
+
+func (b *boolValue) Type() string {
+	return "bool"
+}
+
+func (b *boolValue) String() string { return strconv.FormatBool(bool(*b)) }
+
+// BoolVarP is like BoolVar, but accepts a shorthand letter that can be used
+// after a single dash.
+func BoolVarP(p *bool, name, shorthand string, value bool, usage string) *pflag.Flag {
+	flag := VarPF(newBoolValue(value, p), name, shorthand, usage)
+	flag.NoOptDefVal = "true"
+	return flag
+}
