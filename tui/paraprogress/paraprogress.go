unikraft/kraftkit

commit bfaa6e0b62d13cadce4cb4fcb75596922441f2e6

cmd/kraft|tui/*: Adapt calls to new library versions
Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>

tui/paraprogress/paraprogress.go
@@ -87,8 +87,7 @@ func (pd *ParaProgress) Start() error {
 	tprog = tea.NewProgram(pd, teaOpts...)
 
 	go func() {
-		err := tprog.Start()
-		if err == nil {
+		if _, err := tprog.Run(); err != nil {
 			pd.errChan <- pd.err
 		} else {
 			pd.errChan <- err
@@ -169,7 +168,7 @@ func (md *ParaProgress) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	if complete == len(md.processes) {
 		md.quitting = true
-		return md, tea.Sequentially(tea.Batch(cmds...), tea.Quit)
+		return md, tea.Sequence(tea.Batch(cmds...), tea.Quit)
 	}
 
 	return md, tea.Batch(cmds...)

cmd/kraft/run/run.go
@@ -197,9 +197,7 @@ func (opts *Run) Run(cmd *cobra.Command, args []string) error {
 				return fmt.Errorf("with 'no prompt' enabled please select a target")
 			}
 
-			sp := selection.New("select target:",
-				selection.Choices(project.TargetNames()),
-			)
+			sp := selection.New("select target:", project.TargetNames())
 			sp.Filter = nil
 
 			selectedTarget, err := sp.RunPrompt()
@@ -207,7 +205,7 @@ func (opts *Run) Run(cmd *cobra.Command, args []string) error {
 				return err
 			}
 
-			target = selectedTarget.String
+			target = selectedTarget
 
 		} else if target != "" && utils.Contains(project.TargetNames(), target) {
 			return fmt.Errorf("unknown target: %s", target)

tui/processtree/processtree.go
@@ -146,8 +146,7 @@ func (pt *ProcessTree) Start() error {
 	tprog = tea.NewProgram(pt, teaOpts...)
 
 	go func() {
-		err := tprog.Start()
-		if err == nil {
+		if _, err := tprog.Run(); err != nil {
 			pt.errChan <- pt.err
 		} else {
 			pt.errChan <- err

commit 222af7fac70e9e735e0f442d7bd3dddec97c5747

tui/paraprogress: Handle nil coming out of batched commands
This fixes a crash for all commands caused by bfaa6e0b62d13c.
The crash happens inside a dependency and is caused by the
the combination of `Batch` and `Sequence` functions.
`Batch` can now return `nil` which needs to be handled.

Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>

tui/paraprogress/paraprogress.go
@@ -168,7 +168,11 @@ func (md *ParaProgress) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	if complete == len(md.processes) {
 		md.quitting = true
-		return md, tea.Sequence(tea.Batch(cmds...), tea.Quit)
+		batch := tea.Batch(cmds...)
+		if batch == nil {
+			return md, tea.Quit
+		}
+		return md, tea.Sequence(batch, tea.Quit)
 	}
 
 	return md, tea.Batch(cmds...)