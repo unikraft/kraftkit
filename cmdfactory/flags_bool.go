9ba8e401a8a5251bafd0e121dfbe326bfd632b3f
Alexander Jung <alex@unikraft.io>
2023-01-25 13:28:00 +0100

    cmdfactory: Add `BoolVar`
    BoolVar returns an instantiated flag for to an associated pointer
    boolean value with a given name, default value and usage line.
    
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- cmdfactory/flags_bool.go
+++ cmdfactory/flags_bool.go
@@ -31,6 +31,14 @@ func (b *boolValue) Type() string {
 
 func (b *boolValue) String() string { return strconv.FormatBool(bool(*b)) }
 
+// BoolVar returns an instantiated flag for to an associated pointer boolean
+// value with a given name, default value and usage line.
+func BoolVar(p *bool, name string, value bool, usage string) *pflag.Flag {
+	flag := VarF(newBoolValue(value, p), name, usage)
+	flag.NoOptDefVal = "true"
+	return flag
+}
+
 // BoolVarP is like BoolVar, but accepts a shorthand letter that can be used
 // after a single dash.
 func BoolVarP(p *bool, name, shorthand string, value bool, usage string) *pflag.Flag {
