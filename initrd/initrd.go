fbf7c59a78d0f251424ed932460b565a22a45f31
Alexander Jung <alex@unikraft.io>
2023-04-21 14:35:08 +0000

    WIP

--- initrd/initrd.go
+++ initrd/initrd.go
@@ -74,6 +74,89 @@ func (i *InitrdConfig) NewReader(w io.Reader) (*cpio.Reader, error) {
 	}
 }
 
+// Files returns a map of files within the archive pointing to their absolute
+// path on the host.
+func (i *InitrdConfig) Files() (map[string]string, error) {
+	files := make(map[string]string)
+
+	for _, include := range i.Input {
+		s := strings.Split(include, InputDelimeter)
+		if len(s) > 2 {
+			return nil, fmt.Errorf("invalid input format: expected \"host:archive\" and received: %s", include)
+		}
+
+		onHost, inArchive := i.RelativePath(s[0]), s[1]
+
+		f, err := os.Stat(onHost)
+		if err != nil {
+			return nil, err
+		}
+
+		if f.IsDir() {
+			if err := filepath.Walk(onHost, func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+
+				files[filepath.Join(inArchive, path)] = filepath.Join(onHost, path)
+
+				return nil
+			}); err != nil {
+				return nil, err
+			}
+		} else {
+			files[inArchive] = onHost
+		}
+	}
+
+	return files, nil
+}
+
+// Build generates a new CPIO archive
+func (i *InitrdConfig) Build(overwrite bool) error {
+	if len(i.Output) == 0 {
+		return fmt.Errorf("output path is required")
+	}
+
+	exists, err := os.Stat(i.Output)
+	if err == nil && exists.IsDir() {
+		return fmt.Errorf("cannot build CPIO archive as output cannot be directory")
+
+		// If file already exists, is it a valid CPIO archive?
+	} else if err == nil && !overwrite {
+		file, err := os.Open(i.Output)
+		if err != nil {
+			return err
+		}
+
+		defer file.Close()
+
+		reader, err := i.NewReader(file)
+		if err != nil {
+			return err
+		}
+
+		// Read the first header
+		_, err = reader.Next()
+		if err == cpio.ErrHeader {
+			return fmt.Errorf("existing output archive is invalid: %s: %s", i.Output, err)
+		}
+
+		return nil
+	}
+
+	file, err := os.OpenFile(i.Output, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o644)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	// writer, err := i.NewWriter(file)
+
+	return nil
+}
+
 // RelativePath resolve a relative path based the working directory
 func (i *InitrdConfig) RelativePath(path string) string {
 	if path[0] == '~' {
@@ -114,6 +197,8 @@ func ParseInitrdConfig(workdir string, value string) (*InitrdConfig, error) {
 			fmt.Sprintf("%s:%s", workdir, split[0]),
 		}
 		initrd.Output = initrd.RelativePath(split[1])
+
+		// TODO: if -z
 	} else if len(split) > 2 {
 		return initrd, fmt.Errorf("unexpected initrd format, expected path:file expression")
 	}
