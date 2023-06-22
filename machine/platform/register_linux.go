83030379c824f1f6fe92ce77f2b17457e2ee0326
Alexander Jung <alex@unikraft.io>
2023-06-22 12:16:24 +0000

    feat(machine): Introduce native Firecracker support
    This commit introduces the Firecracker platform into KraftKit that
    facilitates the instantiation and lifecycle management of Unikraft VMs
    running on Firecracker.
    
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- machine/platform/register_linux.go
+++ machine/platform/register_linux.go
@@ -13,6 +13,8 @@ import (
 
 	machinev1alpha1 "kraftkit.sh/api/machine/v1alpha1"
 	"kraftkit.sh/config"
+	"kraftkit.sh/internal/set"
+	"kraftkit.sh/machine/firecracker"
 	"kraftkit.sh/machine/qemu"
 	"kraftkit.sh/machine/store"
 )
@@ -74,6 +76,33 @@ var qemuV1alpha1Driver = func(ctx context.Context, opts ...any) (machinev1alpha1
 	)
 }
 
+var firecrackerV1alpha1Driver = func(ctx context.Context, opts ...any) (machinev1alpha1.MachineService, error) {
+	if set.NewStringSet("debug", "trace").Contains(config.G[config.KraftKit](ctx).Log.Level) {
+		opts = append(opts, firecracker.WithDebug(true))
+	}
+	service, err := firecracker.NewMachineV1alpha1Service(ctx, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	embeddedStore, err := store.NewEmbeddedStore[machinev1alpha1.MachineSpec, machinev1alpha1.MachineStatus](
+		filepath.Join(
+			config.G[config.KraftKit](ctx).RuntimeDir,
+			"machinev1alpha1",
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return machinev1alpha1.NewMachineServiceHandler(
+		ctx,
+		service,
+		zip.WithStore[machinev1alpha1.MachineSpec, machinev1alpha1.MachineStatus](embeddedStore, zip.StoreRehydrationSpecNil),
+		zip.WithBefore(storePlatformFilter(PlatformFirecracker)),
+	)
+}
+
 // hostSupportedStrategies returns the map of known supported drivers for the
 // given host.
 func hostSupportedStrategies() map[Platform]*Strategy {
@@ -83,6 +112,9 @@ func hostSupportedStrategies() map[Platform]*Strategy {
 		},
 		PlatformKVM: {
 			NewMachineV1alpha1: qemuV1alpha1Driver,
+		},
+		PlatformFirecracker: {
+			NewMachineV1alpha1: firecrackerV1alpha1Driver,
 		},
 	}
 }
