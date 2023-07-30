1ea07d7c3b6a5cf1b5d81f17d2f52dfaecf492b7
Alexander Jung <alex@unikraft.io>
2023-07-30 10:54:34 +0000

    refactor(qemu): Move qemu-system-* binary constant names to own file
    Signed-off-by: Alexander Jung <alex@unikraft.io>

---
+++ machine/qemu/qemu_system.go
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022, Unikraft GmbH and The KraftKit Authors.
+// Licensed under the BSD-3-Clause License (the "License").
+// You may not use this file except in compliance with the License.
+package qemu
+
+const (
+	QemuSystemX86     = "qemu-system-x86_64"
+	QemuSystemArm     = "qemu-system-arm"
+	QemuSystemAarch64 = "qemu-system-aarch64"
+)
