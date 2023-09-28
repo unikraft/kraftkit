95f7015af6e99e880b31cb0837908f948f20c8a3
Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>
2023-09-28 16:25:00 +0300

    feat(machine): Split machine detection
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>

---
+++ machine/platform/detect_windows.go
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022, Unikraft GmbH and The KraftKit Authors.
+// Licensed under the BSD-3-Clause License (the "License").
+// You may not use this file except in compliance with the License.
+package platform
+
+import (
+	"context"
+	"fmt"
+)
+
+// Detect returns the hypervisor and system mode in the context to the
+// determined hypervisor or an error if not detectable.
+func Detect(ctx context.Context) (Platform, SystemMode, error) {
+	return PlatformUnknown, SystemUnknown, fmt.Errorf("Hypervisor detection is not supported on Windows")
+}
