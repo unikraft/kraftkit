c265ad3837ebb6e102a9e0ea15a363f21666eb52
Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>
2023-10-02 17:29:03 +0300

    feat: Register NetBSD & OpenBSD as valid platforms
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>

---
+++ machine/platform/register_openbsd.go
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022, Unikraft GmbH and The KraftKit Authors.
+// Licensed under the BSD-3-Clause License (the "License").
+// You may not use this file except in compliance with the License.
+package platform
+
+func unixVariantStrategies() map[Platform]*Strategy {
+	return map[Platform]*Strategy{}
+}
