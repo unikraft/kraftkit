07103edc36b6401397e907017b8c27c3aa28437e
Alexander Jung <alex@unikraft.io>
2023-10-11 16:25:39 +0200

    feat(oci): Introduce compatibility check helper methods
    These methods determine if the provide source is compatible with
    KraftKit, thus abstracting away the check in the attempt to reduce the
    noise across the codebase for the same check.
    
    Signed-off-by: Alexander Jung <alex@unikraft.io>

---
+++ oci/compatible.go
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022, Unikraft GmbH and The KraftKit Authors.
+// Licensed under the BSD-3-Clause License (the "License").
+// You may not use this file except in compliance with the License.
+package oci
+
+import (
+	"fmt"
+
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+)
+
+// IsOCIManifestKraftKitCompatible is a utility method that is used to determine
+// whether the provided OCI Specification Manifest structure is compatible with
+// KraftKit.  Ultimately, this is achieved by testing whether the annotation
+// "sh.kraftkit.version" has been set.  The value of this annotation is
+// discarded.
+func IsOCIManifestKraftKitCompatible(manifest *ocispec.Manifest) (bool, error) {
+	if manifest == nil {
+		return false, fmt.Errorf("provided manifest is nil")
+	}
+
+	if manifest.Annotations == nil {
+		return false, fmt.Errorf("manifest does not contain any annotations")
+	}
+
+	if _, ok := manifest.Annotations[AnnotationKraftKitVersion]; !ok {
+		return false, fmt.Errorf("manifest does not contain '%s' annotation", AnnotationKraftKitVersion)
+	}
+
+	return true, nil
+}
+
+// IsOCIDescriptorKraftKitCompatible is a utility method that is used to
+// determine whether the provided OCI Specification Descriptor structure is
+// compatible with KraftKit.  Ultimately, this is achieved by testing whether
+// the annotation "sh.kraftkit.version" has been set.  The value of this
+// annotation is discarded.
+func IsOCIDescriptorKraftKitCompatible(descriptor *ocispec.Descriptor) (bool, error) {
+	if descriptor == nil {
+		return false, fmt.Errorf("provided descriptor is nil")
+	}
+
+	if descriptor.Annotations == nil {
+		return false, fmt.Errorf("descriptor does not contain any annotations")
+	}
+
+	if _, ok := descriptor.Annotations[AnnotationKraftKitVersion]; !ok {
+		return false, fmt.Errorf("descriptor does not contain '%s' annotation", AnnotationKraftKitVersion)
+	}
+
+	return true, nil
+}
