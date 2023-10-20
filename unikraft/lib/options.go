e0b33bca8976ebbab570cce19dc1045bb685a6e1
Luca Seritan <luca.seritan@gmail.com>
2023-10-20 16:01:43 +0300

    feat(lib): Construct library from options and add uklibid fields
    This commit introduces the ability to construct a LibraryConfig
    using options. Additionally, it adds new fields, getters and
    options for the fields introduces by uklibid.
    
    Signed-off-by: Luca Seritan <luca.seritan@gmail.com>

--- unikraft/lib/options.go
+++ unikraft/lib/options.go
@@ -4,6 +4,8 @@
 // You may not use this file except in compliance with the License.
 package lib
 
+import "kraftkit.sh/make"
+
 type LibraryOption func(*LibraryConfig) error
 
 // WithInternal marks the library as a part of the Unikraft core.
@@ -35,6 +37,54 @@ func WithSource(source string) LibraryOption {
 func WithVersion(version string) LibraryOption {
 	return func(lc *LibraryConfig) error {
 		lc.version = version
+		return nil
+	}
+}
+
+// WithCFlags sets the C flags of this library component.
+func WithCFlags(cflags []*make.ConditionalValue) LibraryOption {
+	return func(lc *LibraryConfig) error {
+		lc.cflags = cflags
+		return nil
+	}
+}
+
+// WithLicense sets the license of this library component.
+func WithLicense(license string) LibraryOption {
+	return func(lc *LibraryConfig) error {
+		lc.license = license
+		return nil
+	}
+}
+
+// WithCompiler sets the compiler of this library component.
+func WithCompiler(compiler string) LibraryOption {
+	return func(lc *LibraryConfig) error {
+		lc.compiler = compiler
+		return nil
+	}
+}
+
+// WithCompileDate sets the compile date of this library component.
+func WithCompileDate(compileDate string) LibraryOption {
+	return func(lc *LibraryConfig) error {
+		lc.compileDate = compileDate
+		return nil
+	}
+}
+
+// WithCompiledBy sets the user who compiled this library component.
+func WithCompiledBy(compiledBy string) LibraryOption {
+	return func(lc *LibraryConfig) error {
+		lc.compiledBy = compiledBy
+		return nil
+	}
+}
+
+// WithCompiledByAssoc sets the association of the user who compiled this library component.
+func WithCompiledByAssoc(compiledByAssoc string) LibraryOption {
+	return func(lc *LibraryConfig) error {
+		lc.compiledByAssoc = compiledByAssoc
 		return nil
 	}
 }
