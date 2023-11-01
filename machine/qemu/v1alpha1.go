9cd70798af7847259ffb29a57aede82d59c2ae85
Alexander Jung <alex@unikraft.io>
2023-11-01 16:33:07 +0100

    feat(qemu): Add `initrd` volume vfstab entry
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- machine/qemu/v1alpha1.go
+++ machine/qemu/v1alpha1.go
@@ -358,6 +358,16 @@ func (service *machineV1alpha1Service) Create(ctx context.Context, machine *mach
 				"mkpath",
 			).String())
 
+		case "initrd":
+			fstab = append(fstab, vfscore.NewFstabEntry(
+				"initrd",
+				vol.Spec.Destination,
+				vol.Spec.Driver,
+				"",
+				"",
+				// By default, create the directory if it does not exist when mounting.
+				"mkpath",
+			).String())
 		default:
 			return machine, fmt.Errorf("unsupported QEMU volume driver: %v", vol.Spec.Driver)
 		}
