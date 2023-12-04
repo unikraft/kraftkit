unikraft/kraftkit

commit bfecc9a99bd6fe28246ccc4a20191787504370dd

build: Enable static PIE builds on all binaries
Signed-off-by: Alexander Jung <alex@unikraft.io>

Makefile
@@ -111,7 +111,7 @@ all: help
 ifeq ($(DEBUG),y)
 $(addprefix $(.PROXY), $(BIN)): GO_GCFLAGS ?= -N -l
 else
-$(addprefix $(.PROXY), $(BIN)): GO_LDFLAGS ?= -s -w
+$(addprefix $(.PROXY), $(BIN)): GO_LDFLAGS ?= -s -w -linkmode external -extldflags '-static-pie'
 endif
 $(addprefix $(.PROXY), $(BIN)): GO_LDFLAGS += -X "$(GOMOD)/internal/version.version=$(VERSION)"
 $(addprefix $(.PROXY), $(BIN)): GO_LDFLAGS += -X "$(GOMOD)/internal/version.commit=$(GIT_SHA)"
@@ -121,6 +121,7 @@ $(addprefix $(.PROXY), $(BIN)):
 	GOOS=$(GOOS) \
 	GOARCH=$(GOARCH) \
 	$(GO) build \
+		-buildmode=pie \
 		-gcflags=all='$(GO_GCFLAGS)' \
 		-ldflags='$(GO_LDFLAGS)' \
 		-o $(DISTDIR)/$@ \

commit f63b39fde2f76c71c90c85a3f82cfedd9fb00004

build: Remove linkmode external and and static-pie linking
This commit partially reverts bfecc9a which introduced compatibility
issues with the portability of the binary.  However, the buildmode
is kept as PIE.

Signed-off-by: Alexander Jung <alex@unikraft.io>

Makefile
@@ -111,7 +111,7 @@ all: help
 ifeq ($(DEBUG),y)
 $(addprefix $(.PROXY), $(BIN)): GO_GCFLAGS ?= -N -l
 else
-$(addprefix $(.PROXY), $(BIN)): GO_LDFLAGS ?= -s -w -linkmode external -extldflags '-static-pie'
+$(addprefix $(.PROXY), $(BIN)): GO_LDFLAGS ?= -s -w
 endif
 $(addprefix $(.PROXY), $(BIN)): GO_LDFLAGS += -X "$(GOMOD)/internal/version.version=$(VERSION)"
 $(addprefix $(.PROXY), $(BIN)): GO_LDFLAGS += -X "$(GOMOD)/internal/version.commit=$(GIT_SHA)"