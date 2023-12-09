d8a224370f7534162f4a8bf7e6dfb58521cd6bb7
Alexander Jung <alex@unikraft.io>
2023-12-09 09:48:49 +0100

    fix(machine): Use correct Unikraft vfstap mount option name
    Following the merge of [0], the name of the Unikraft-specific
    VFSTAB cli option for creating a non-existing directory is called
    `mkmp` and not `mkpath`.  The latter was the original name which
    was changed during the review process of [0].
    
    [0]: https://github.com/unikraft/unikraft/pull/1145
    
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- machine/qemu/v1alpha1.go
+++ machine/qemu/v1alpha1.go
@@ -355,7 +355,7 @@ func (service *machineV1alpha1Service) Create(ctx context.Context, machine *mach
 				"",
 				"",
 				// By default, create the directory if it does not exist when mounting.
-				"mkpath",
+				"mkmp",
 			).String())
 
 		case "initrd":
@@ -366,7 +366,7 @@ func (service *machineV1alpha1Service) Create(ctx context.Context, machine *mach
 				"",
 				"",
 				// By default, create the directory if it does not exist when mounting.
-				"mkpath",
+				"mkmp",
 			).String())
 		default:
 			return machine, fmt.Errorf("unsupported QEMU volume driver: %v", vol.Spec.Driver)
