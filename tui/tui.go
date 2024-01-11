5470cf95e26ef0db68bc1e6f30e976000b289309
Alexander Jung <alex@unikraft.io>
2024-01-11 12:33:07 +0100

    feat(tui): Introduce `TextRed`, `TextGreen`, `TextBlue` and `TextYellow`
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- tui/tui.go
+++ tui/tui.go
@@ -11,6 +11,10 @@ var (
 			Bold(true).
 			Render
 
+	TextRed = lipgloss.NewStyle().
+		Foreground(lipgloss.Color("9")).
+		Render
+
 	TextWhiteBgRed = lipgloss.NewStyle().
 			Background(lipgloss.Color("9")).
 			Foreground(lipgloss.AdaptiveColor{
@@ -19,6 +23,10 @@ var (
 		}).
 		Render
 
+	TextGreen = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("10")).
+			Render
+
 	TextWhiteBgGreen = lipgloss.NewStyle().
 				Background(lipgloss.Color("10")).
 				Foreground(lipgloss.AdaptiveColor{
@@ -26,6 +34,10 @@ var (
 			Dark:  "15",
 		}).
 		Render
+
+	TextBlue = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("12")).
+			Render
 
 	TextWhiteBgBlue = lipgloss.NewStyle().
 			Background(lipgloss.Color("12")).
@@ -37,5 +49,9 @@ var (
 
 	TextLightGray = lipgloss.NewStyle().
 			Foreground(lipgloss.Color("245")).
+			Render
+
+	TextYellow = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("11")).
 			Render
 )
