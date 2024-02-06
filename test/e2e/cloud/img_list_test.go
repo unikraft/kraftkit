6cfac6a7e730d223599c4eab18bafbf4109cd19d
Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>
2024-02-06 18:31:05 +0200

    feat(test): Add initial kraft cloud tests
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>

---
+++ test/e2e/cloud/img_list_test.go
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024, Unikraft GmbH and The KraftKit Authors.
+// Licensed under the BSD-3-Clause License (the "License").
+// You may not use this file except in compliance with the License.
+
+package cli_test
+
+import (
+	"fmt"
+	"os"
+
+	. "github.com/onsi/ginkgo/v2" //nolint:stylecheck
+	. "github.com/onsi/gomega"    //nolint:stylecheck
+
+	fcmd "kraftkit.sh/test/e2e/framework/cmd"
+	fcfg "kraftkit.sh/test/e2e/framework/config"
+)
+
+var _ = Describe("kraft cloud img ls", func() {
+	var cmd *fcmd.Cmd
+
+	var stdout *fcmd.IOStream
+	var stderr *fcmd.IOStream
+
+	var cfg *fcfg.Config
+
+	BeforeEach(func() {
+		if os.Getenv("KRAFTCLOUD_TOKEN") == "" {
+			Skip("KRAFTCLOUD_TOKEN is not set")
+		}
+
+		if os.Getenv("KRAFTCLOUD_METRO") == "" {
+			Skip("KRAFTCLOUD_METRO is not set")
+		}
+
+		stdout = fcmd.NewIOStream()
+		stderr = fcmd.NewIOStream()
+
+		cfg = fcfg.NewTempConfig()
+
+		cmd = fcmd.NewKraft(stdout, stderr, cfg.Path())
+		cmd.Env = os.Environ()
+		cmd.Args = append(cmd.Args, "cloud", "img", "ls", "--log-level", "info", "--log-type", "json", "-o", "json")
+	})
+
+	When("invoked with the --help flag", func() {
+		BeforeEach(func() {
+			cmd.Args = append(cmd.Args, "--help")
+		})
+
+		It("should print the command's help", func() {
+			err := cmd.Run()
+			if err != nil {
+				fmt.Print(cmd.DumpError(stdout, stderr, err))
+			}
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(stderr.String()).To(BeEmpty())
+			Expect(stdout.String()).To(MatchRegexp(`List all images in your account.`))
+		})
+	})
+})
