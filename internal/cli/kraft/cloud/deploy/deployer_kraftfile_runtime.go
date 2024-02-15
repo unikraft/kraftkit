c635dee7e3a4b53bd3ef00f2f6131b492275d7ac
Alexander Jung <alex@unikraft.io>
2024-02-15 16:24:56 +0100

    feat(deploy): Set `NoPull` to true for kraft cloud images
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- internal/cli/kraft/cloud/deploy/deployer_kraftfile_runtime.go
+++ internal/cli/kraft/cloud/deploy/deployer_kraftfile_runtime.go
@@ -89,6 +89,7 @@ func (deployer *deployerKraftfileRuntime) Deploy(ctx context.Context, opts *Depl
 		Format:       "oci",
 		Kraftfile:    opts.Kraftfile,
 		Name:         pkgName,
+		NoPull:       true,
 		Platform:     "kraftcloud",
 		Project:      opts.Project,
 		Push:         true,
