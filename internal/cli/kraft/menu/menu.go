9dc243b639fdf648b8abc8efa4653dff96f76f6e
Alexander Jung <alex@unikraft.io>
2024-02-20 15:03:40 +0100

    fix(menu): Handle too many options with selection and better error
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- internal/cli/kraft/menu/menu.go
+++ internal/cli/kraft/menu/menu.go
@@ -26,6 +26,7 @@ import (
 	"kraftkit.sh/make"
 	"kraftkit.sh/packmanager"
 	"kraftkit.sh/tui/paraprogress"
+	"kraftkit.sh/tui/selection"
 	"kraftkit.sh/unikraft/app"
 	"kraftkit.sh/unikraft/target"
 
@@ -119,6 +120,8 @@ func (opts *MenuOptions) pull(ctx context.Context, project app.Application, work
 
 	if _, err := opts.project.Components(ctx); err != nil && opts.project.Template().Name() != "" {
 		var packages []pack.Package
+		var pullPack pack.Package
+
 		search := processtree.NewProcessTreeItem(
 			fmt.Sprintf("finding %s",
 				unikraft.TypeNameVersion(opts.project.Template()),
@@ -137,10 +140,6 @@ func (opts *MenuOptions) pull(ctx context.Context, project app.Application, work
 
 				if len(packages) == 0 {
 					return fmt.Errorf("could not find: %s",
-						unikraft.TypeNameVersion(opts.project.Template()),
-					)
-				} else if len(packages) > 1 {
-					return fmt.Errorf("too many options for %s",
 						unikraft.TypeNameVersion(opts.project.Template()),
 					)
 				}
@@ -166,12 +165,35 @@ func (opts *MenuOptions) pull(ctx context.Context, project app.Application, work
 			return fmt.Errorf("could not complete search: %v", err)
 		}
 
+		if len(packages) == 1 {
+			pullPack = packages[0]
+		} else if len(packages) > 1 {
+			if config.G[config.KraftKit](ctx).NoPrompt {
+				for _, p := range packages {
+					log.G(ctx).
+						WithField("template", p.String()).
+						Warn("possible")
+				}
+
+				return fmt.Errorf("too many options for %s and prompting has been disabled",
+					opts.project.Template().String(),
+				)
+			}
+
+			selected, err := selection.Select[pack.Package]("select possible template", packages...)
+			if err != nil {
+				return err
+			}
+
+			pullPack = *selected
+		}
+
 		proc := paraprogress.NewProcess(
 			fmt.Sprintf("pulling %s",
-				unikraft.TypeNameVersion(packages[0]),
+				unikraft.TypeNameVersion(pullPack),
 			),
 			func(ctx context.Context, w func(progress float64)) error {
-				return packages[0].Pull(
+				return pullPack.Pull(
 					ctx,
 					pack.WithPullProgressFunc(w),
 					pack.WithPullWorkdir(workdir),
