14815b7e8d97f2cba220f5f6d7221d14d9904cc6
Alexander Jung <alex@unikraft.io>
2024-02-20 15:23:09 +0100

    fix(deploy): Pass volumes CLI arg
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- internal/cli/kraft/cloud/deploy/deploy.go
+++ internal/cli/kraft/cloud/deploy/deploy.go
@@ -57,6 +57,7 @@ type DeployOptions struct {
 	Strategy               packmanager.MergeStrategy `noattribute:"true"`
 	SubDomain              string                    `local:"true" long:"subdomain" short:"s" usage:"Set the name to use when provisioning a subdomain"`
 	Timeout                time.Duration             `local:"true" long:"timeout" usage:"Set the timeout for remote procedure calls"`
+	Volumes                []string                  `long:"volume" short:"v" usage:"Specify the volume mapping(s) in the form NAME:DEST or NAME:DEST:OPTIONS"`
 	Workdir                string                    `local:"true" long:"workdir" short:"w" usage:"Set an alternative working directory (default is cwd)"`
 }
 
