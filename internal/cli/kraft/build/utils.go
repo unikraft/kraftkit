2ce9df69844e1f158bdf976ed037e4dc361123af
Alexander Jung <alex@unikraft.io>
2024-03-08 16:24:55 +0100

    feat(build): Introduce utility method to `initProject`
    Signed-off-by: Alexander Jung <alex@unikraft.io>

---
+++ internal/cli/kraft/build/utils.go
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2023, Unikraft GmbH and The KraftKit Authors.
+// Licensed under the BSD-3-Clause License (the "License").
+// You may not use this file except in compliance with the License.
+
+package build
+
+import (
+	"context"
+
+	"kraftkit.sh/unikraft/app"
+)
+
+// initProject sets up the project based on the provided context and
+// options.
+func (opts *BuildOptions) initProject(ctx context.Context) error {
+	var err error
+
+	popts := []app.ProjectOption{
+		app.WithProjectWorkdir(opts.Workdir),
+	}
+
+	if len(opts.Kraftfile) > 0 {
+		popts = append(popts, app.WithProjectKraftfile(opts.Kraftfile))
+	} else {
+		popts = append(popts, app.WithProjectDefaultKraftfiles())
+	}
+
+	// Interpret the project directory
+	opts.project, err = app.NewProjectFromOptions(ctx, popts...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
