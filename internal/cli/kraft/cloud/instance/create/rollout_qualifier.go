1c70b2733a8dd5da8e78f9d311bac59673d3e32d
Alexander Jung <alex@unikraft.io>
2024-04-04 18:11:52 +0200

    feat(create): Introduce `RolloutQualifier`
    The "rollout qualifier" is the detection mechanism used to
    determine whether the specific instance should be affected by the
    rollout strategy.
    
    Signed-off-by: Alexander Jung <alex@unikraft.io>

---
+++ internal/cli/kraft/cloud/instance/create/rollout_qualifier.go
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024, Unikraft GmbH and The KraftKit Authors.
+// Licensed under the BSD-3-Clause License (the "License").
+// You may not use this file except in compliance with the License.
+
+package create
+
+import "fmt"
+
+// RolloutQualifier is the detection mechanism used to determine whether the
+// specific instance should be affected by the rollout strategy.
+type RolloutQualifier string
+
+const (
+	// The 'image' qualifier is used to capture instances which have the same
+	// image (ignoring digest).
+	RolloutQualifierImageName = RolloutQualifier("image")
+
+	// The 'name' qualifier is used to capture instances which have the same name.
+	RolloutQualifierInstanceName = RolloutQualifier("name")
+
+	// The 'all' qualifier matches all instances in the service group.
+	RolloutQualifierAll = RolloutQualifier("all")
+
+	// The 'none' qualifier prevents matching any instances in the service group.
+	RolloutQualifierNone = RolloutQualifier("none")
+)
+
+var _ fmt.Stringer = (*RolloutQualifier)(nil)
+
+// String implements fmt.Stringer
+func (strategy RolloutQualifier) String() string {
+	return string(strategy)
+}
+
+// RolloutQualifiers returns the list of possible rollout qualifier.
+func RolloutQualifiers() []RolloutQualifier {
+	return []RolloutQualifier{
+		RolloutQualifierImageName,
+		RolloutQualifierInstanceName,
+		RolloutQualifierAll,
+		RolloutQualifierNone,
+	}
+}
+
+// RolloutQualifierNames returns the string representation of all possible
+// rollout qualifiers.
+func RolloutQualifierNames() []string {
+	qualifiers := []string{}
+	for _, strategy := range RolloutQualifiers() {
+		qualifiers = append(qualifiers, strategy.String())
+	}
+
+	return qualifiers
+}
