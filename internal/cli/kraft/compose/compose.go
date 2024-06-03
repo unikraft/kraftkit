1484baa97bfdcb48073594f3b4cf0788e51f6d44
Luca Seritan <luca.seritan@gmail.com>
2024-06-03 11:57:24 +0300

    feat(compose): Introduce the pull subcommand
    Signed-off-by: Luca Seritan <luca.seritan@gmail.com>

--- internal/cli/kraft/compose/compose.go
+++ internal/cli/kraft/compose/compose.go
@@ -20,6 +20,7 @@ import (
 	"kraftkit.sh/internal/cli/kraft/compose/ls"
 	"kraftkit.sh/internal/cli/kraft/compose/pause"
 	"kraftkit.sh/internal/cli/kraft/compose/ps"
+	"kraftkit.sh/internal/cli/kraft/compose/pull"
 	"kraftkit.sh/internal/cli/kraft/compose/start"
 	"kraftkit.sh/internal/cli/kraft/compose/stop"
 	"kraftkit.sh/internal/cli/kraft/compose/unpause"
@@ -58,6 +59,7 @@ func NewCmd() *cobra.Command {
 	cmd.AddCommand(ls.NewCmd())
 	cmd.AddCommand(pause.NewCmd())
 	cmd.AddCommand(ps.NewCmd())
+	cmd.AddCommand(pull.NewCmd())
 	cmd.AddCommand(start.NewCmd())
 	cmd.AddCommand(stop.NewCmd())
 	cmd.AddCommand(unpause.NewCmd())
