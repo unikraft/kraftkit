418ca3f5529a67e1011f2499b9700d91936a4752
Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>
2024-09-16 14:52:02 +0300

    feat(cloud): Add option to prompt select metro
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>

--- internal/cli/kraft/cloud/utils/populate_flags.go
+++ internal/cli/kraft/cloud/utils/populate_flags.go
@@ -5,8 +5,23 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"kraftkit.sh/config"
 	"kraftkit.sh/log"
+	"kraftkit.sh/tui/selection"
+
+	unikraftcloud "sdk.kraft.cloud"
 )
+
+type Metro struct {
+	Location string
+	Code     string
+}
+
+var _ fmt.Stringer = (*Metro)(nil)
+
+func (m Metro) String() string {
+	return fmt.Sprintf("%s (%s)", m.Code, m.Location)
+}
 
 func PopulateMetroToken(cmd *cobra.Command, metro, token *string) error {
 	*metro = cmd.Flag("metro").Value.String()
@@ -23,6 +38,30 @@ func PopulateMetroToken(cmd *cobra.Command, metro, token *string) error {
 
 		if *metro == "" {
 			*metro = os.Getenv("UKC_METRO")
+		}
+
+		if *metro == "" && !config.G[config.KraftKit](cmd.Context()).NoPrompt {
+			client := unikraftcloud.NewMetrosClient()
+
+			metros, err := client.List(cmd.Context(), false)
+			if err != nil {
+				return fmt.Errorf("could not list metros: %w", err)
+			}
+
+			candidates := make([]Metro, len(metros))
+			for i, m := range metros {
+				candidates[i].Code = m.Code
+				candidates[i].Location = m.Location
+			}
+
+			candidate, err := selection.Select("metro not explicitly set: which one would you like to use?", candidates...)
+			if err != nil {
+				return err
+			}
+
+			*metro = candidate.Code
+
+			log.G(cmd.Context()).Infof("run `export UKC_METRO=%s` or use the `--metro` flag to skip this prompt in the future", *metro)
 		}
 
 		if *metro == "" {
