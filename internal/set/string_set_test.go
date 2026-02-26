552187e18e8f25288d8865ae55bf5d4fba92532d
kami-sama <kamisamakirishima@gmail.com>
2026-02-26 10:47:54 +0530

    test(internal/set): Added remaining tests for this package
    Added test for Add, Remove, Equal methods and NewStringSet and
    sliceWithout function
    
    Signed-off-by: kami-sama <kamisamakirishima@gmail.com>

--- internal/set/string_set_test.go
+++ internal/set/string_set_test.go
@@ -18,7 +18,7 @@ func TestContainsAnyOf(t *testing.T) {
 		want   bool
 	}{
 		{
-			name:   "nil values",
+			name:   "nil args",
 			args:   nil,
 			fields: []string{"one", "two"},
 		},
@@ -81,7 +81,7 @@ func TestContainsExactlyAnyOf(t *testing.T) {
 		want   bool
 	}{
 		{
-			name:   "nil values",
+			name:   "nil args",
 			args:   nil,
 			fields: []string{"one", "two"},
 		},
@@ -235,5 +235,347 @@ func TestToSlice(t *testing.T) {
 	want = []string{"one", "two"}
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("ToSlice() = %v, want %v", got, want)
+	}
+}
+
+func TestEqual(t *testing.T) {
+	tests := []struct {
+		name   string
+		args   []string
+		fields []string
+		want   bool
+	}{
+		{
+			name:   "nil args",
+			args:   nil,
+			fields: []string{"one", "two"},
+		},
+		{
+			name:   "empty args",
+			args:   []string{},
+			fields: []string{"one", "two"},
+		},
+		{
+			name:   "empty stringSet",
+			args:   []string{"one", "two"},
+			fields: []string{},
+		},
+		{
+			name:   "equal in order",
+			args:   []string{"one", "two", "three"},
+			fields: []string{"one", "two", "three"},
+			want:   true,
+		},
+		{
+			name:   "equal out of order",
+			args:   []string{"three", "one", "two"},
+			fields: []string{"one", "two", "three"},
+			want:   true,
+		},
+		{
+			name:   "case sensitivity",
+			args:   []string{"ONE", "tWo", "three"},
+			fields: []string{"one", "two", "three"},
+		},
+		{
+			name:   "partial set match",
+			args:   []string{"one", "three"},
+			fields: []string{"one", "two", "three"},
+		},
+		{
+			name:   "partial string match",
+			args:   []string{"on", "wo", "hree"},
+			fields: []string{"one", "two", "three"},
+		},
+		{
+			name:   "emoji",
+			args:   []string{"😀", "💀🐹", "🐹"},
+			fields: []string{"😀", "💀🐹", "🐹"},
+			want:   true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s1 := NewStringSet(tt.fields...)
+			s2 := NewStringSet(tt.args...)
+			if got := s1.Equal(s2); got != tt.want {
+				t.Errorf("Equal() = %t, want %t", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSliceWithout(t *testing.T) {
+	tests := []struct {
+		name    string
+		args    []string
+		without string
+		want    []string
+	}{
+		{
+			name:    "nil args",
+			args:    nil,
+			without: "",
+			want:    nil,
+		},
+		{
+			name:    "empty args",
+			args:    []string{},
+			without: "",
+			want:    []string{},
+		},
+		{
+			name:    "remove first",
+			args:    []string{"one", "two", "three"},
+			without: "one",
+			want:    []string{"two", "three"},
+		},
+		{
+			name:    "remove from multiple instances",
+			args:    []string{"one", "two", "three", "two"},
+			without: "two",
+			want:    []string{"one", "three", "two"},
+		},
+		{
+			name:    "remove arbitrary from even",
+			args:    []string{"one", "two", "three", "four"},
+			without: "three",
+			want:    []string{"one", "two", "four"},
+		},
+		{
+			name:    "remove arbitrary from odd",
+			args:    []string{"one", "two", "three", "four", "five"},
+			without: "three",
+			want:    []string{"one", "two", "four", "five"},
+		},
+		{
+			name:    "case sensitivity",
+			args:    []string{"one", "two", "three"},
+			without: "ONE",
+			want:    []string{"one", "two", "three"},
+		},
+		{
+			name:    "remove last",
+			args:    []string{"one", "two", "three"},
+			without: "three",
+			want:    []string{"one", "two"},
+		},
+		{
+			name:    "emoji",
+			args:    []string{"😀", "💀🐹", "🐹"},
+			without: "💀🐹",
+			want:    []string{"😀", "🐹"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := sliceWithout(tt.args, tt.without)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sliceWithout() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAdd(t *testing.T) {
+	tests := []struct {
+		name      string
+		initial   []string
+		args      []string
+		wantSlice []string
+	}{
+		{
+			name:      "nil args",
+			args:      nil,
+			initial:   []string{"one", "two"},
+			wantSlice: []string{"one", "two"},
+		},
+		{
+			name:      "empty args",
+			args:      []string{},
+			initial:   []string{"one", "two"},
+			wantSlice: []string{"one", "two"},
+		},
+		{
+			name:      "empty stringSet",
+			args:      []string{"one", "two"},
+			initial:   []string{},
+			wantSlice: []string{"one", "two"},
+		},
+		{
+			name:      "single args",
+			args:      []string{"three"},
+			initial:   []string{"one", "two"},
+			wantSlice: []string{"one", "two", "three"},
+		},
+		{
+			name:      "multiple args",
+			args:      []string{"two", "three"},
+			initial:   []string{"one"},
+			wantSlice: []string{"one", "two", "three"},
+		},
+		{
+			name:      "already exists",
+			args:      []string{"two"},
+			initial:   []string{"one", "two"},
+			wantSlice: []string{"one", "two"},
+		},
+		{
+			name:      "multiple repeating args",
+			args:      []string{"two", "two", "three"},
+			initial:   []string{"one"},
+			wantSlice: []string{"one", "two", "three"},
+		},
+		{
+			name:      "emoji",
+			args:      []string{"🫩"},
+			initial:   []string{"😀", "💀🐹", "🐹", "😇", "😑"},
+			wantSlice: []string{"😀", "💀🐹", "🐹", "😇", "😑", "🫩"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewStringSet(tt.initial...).Add(tt.args...)
+			want := NewStringSet(tt.wantSlice...)
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("Add() = %v, want %v", got, tt.wantSlice)
+			}
+		})
+	}
+}
+
+func TestRemove(t *testing.T) {
+	tests := []struct {
+		name      string
+		initial   []string
+		args      []string
+		wantSlice []string
+	}{
+		{
+			name:      "nil args",
+			args:      nil,
+			initial:   []string{"one", "two"},
+			wantSlice: []string{"one", "two"},
+		},
+		{
+			name:      "empty args",
+			args:      []string{},
+			initial:   []string{"one", "two"},
+			wantSlice: []string{"one", "two"},
+		},
+		{
+			name:      "empty stringSet",
+			args:      []string{"one", "two"},
+			initial:   []string{},
+			wantSlice: []string{},
+		},
+		{
+			name:      "single args",
+			args:      []string{"three"},
+			initial:   []string{"one", "two", "three"},
+			wantSlice: []string{"one", "two"},
+		},
+		{
+			name:      "multiple args",
+			args:      []string{"one", "three"},
+			initial:   []string{"one", "two", "three"},
+			wantSlice: []string{"two"},
+		},
+		{
+			name:      "doesn't exist",
+			args:      []string{"three"},
+			initial:   []string{"one", "two"},
+			wantSlice: []string{"one", "two"},
+		},
+		{
+			name:      "repeating args",
+			args:      []string{"two", "two"},
+			initial:   []string{"one", "two"},
+			wantSlice: []string{"one"},
+		},
+		{
+			name:      "partial match",
+			args:      []string{"tw", "thr"},
+			initial:   []string{"one", "two", "three"},
+			wantSlice: []string{"one", "two", "three"},
+		},
+		{
+			name:      "emoji",
+			args:      []string{"😇"},
+			initial:   []string{"😀", "💀🐹", "🐹", "😇", "😑"},
+			wantSlice: []string{"😀", "💀🐹", "🐹", "😑"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewStringSet(tt.initial...).Remove(tt.args...)
+			want := NewStringSet(tt.wantSlice...)
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("Add() = %v, want %v", got, tt.wantSlice)
+			}
+		})
+	}
+}
+
+func TestNewStringSet(t *testing.T) {
+	tests := []struct {
+		name      string
+		input     []string
+		wantSlice []string
+	}{
+		{
+			name:      "nil args",
+			input:     nil,
+			wantSlice: []string{},
+		},
+		{
+			name:      "empty args",
+			input:     []string{},
+			wantSlice: []string{},
+		},
+		{
+			name:      "single input",
+			input:     []string{"one"},
+			wantSlice: []string{"one"},
+		},
+		{
+			name:      "multiple input",
+			input:     []string{"one", "two"},
+			wantSlice: []string{"one", "two"},
+		},
+		{
+			name:      "repeating input values",
+			input:     []string{"one", "two", "three", "two"},
+			wantSlice: []string{"one", "two", "three"},
+		},
+		{
+			name:      "emoji",
+			input:     []string{"😀", "💀🐹", "🐹", "😑"},
+			wantSlice: []string{"😀", "💀🐹", "🐹", "😑"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewStringSet(tt.input...)
+
+			if !reflect.DeepEqual(got.v, tt.wantSlice) {
+				t.Errorf("NewStringSet() = %v, want %v", got.v, tt.wantSlice)
+			}
+
+			if len(got.m) != len(tt.wantSlice) {
+				t.Errorf("map size %d, expected %d", len(got.m), len(tt.wantSlice))
+			}
+
+			for _, s := range tt.wantSlice {
+				if _, ok := got.m[s]; !ok {
+					t.Errorf("expected value %q in map: %v", s, got.m)
+				}
+			}
+		})
 	}
 }
