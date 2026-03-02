d0b9eb23216c6cc4b55029b8e8ce6d32edaf1eef
Cezar Craciunoiu <cezar@unikraft.io>
2026-03-02 16:43:16 +0200

    fix(pack): Use custom auth per-request
    Signed-off-by: Cezar Craciunoiu <cezar@unikraft.io>

--- pack/push_option.go
+++ pack/push_option.go
@@ -4,10 +4,17 @@
 // You may not use this file except in compliance with the License.
 package pack
 
+import (
+	"maps"
+
+	"kraftkit.sh/config"
+)
+
 // PushOptions contains the list of options which can be set whilst pushing a
 // package.
 type PushOptions struct {
 	onProgress func(progress float64)
+	auths      map[string]config.AuthConfig
 }
 
 // PushOption is an option function which is used to modify PushOptions.
@@ -33,11 +40,31 @@ func (ppo *PushOptions) OnProgress() func(float64) {
 	return ppo.onProgress
 }
 
+// Auths returns the set authentication config for a given domain or nil if the
+// domain was not found.
+func (ppo *PushOptions) Auths() map[string]config.AuthConfig {
+	return ppo.auths
+}
+
 // WithPushProgressFunc set an optional progress function which is used as a
 // callback during the transmission of the package and the host.
 func WithPushProgressFunc(onProgress func(progress float64)) PushOption {
 	return func(opts *PushOptions) error {
 		opts.onProgress = onProgress
+		return nil
+	}
+}
+
+// WithPushAuthConfig sets the authentication config to use when pushing the
+// package.
+func WithPushAuthConfig(auth map[string]config.AuthConfig) PushOption {
+	return func(opts *PushOptions) error {
+		if opts.auths == nil {
+			opts.auths = map[string]config.AuthConfig{}
+		}
+
+		maps.Copy(opts.auths, auth)
+
 		return nil
 	}
 }
