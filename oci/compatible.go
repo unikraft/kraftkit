215d524602a46ed730ab77bb54e7f8a9f4766714
Cezar Craciunoiu <cezar@unikraft.io>
2026-04-29 15:15:04 +0300

    fix(oci): Remove kraftkit-specific annotation checks
    Signed-off-by: Cezar Craciunoiu <cezar@unikraft.io>

--- oci/compatible.go
+++ oci/compatible.go
@@ -12,20 +12,10 @@ import (
 
 // IsOCIIndexKraftKitCompatible is a utility method that is used to determine
 // whether the provided OCI Specification Index structure is compatible with
-// KraftKit.  Ultimately, this is achieved by testing whether the annotation
-// "sh.kraftkit.version" has been set.  The value of this annotation is
-// discarded.
+// KraftKit.
 func IsOCIIndexKraftKitCompatible(index *ocispec.Index) (bool, error) {
 	if index == nil {
 		return false, fmt.Errorf("provided index is nil")
-	}
-
-	if index.Annotations == nil {
-		return false, fmt.Errorf("index does not contain any annotations")
-	}
-
-	if _, ok := index.Annotations[AnnotationKraftKitVersion]; !ok {
-		return false, fmt.Errorf("index does not contain '%s' annotation", AnnotationKraftKitVersion)
 	}
 
 	return true, nil
@@ -33,20 +23,10 @@ func IsOCIIndexKraftKitCompatible(index *ocispec.Index) (bool, error) {
 
 // IsOCIManifestKraftKitCompatible is a utility method that is used to determine
 // whether the provided OCI Specification Manifest structure is compatible with
-// KraftKit.  Ultimately, this is achieved by testing whether the annotation
-// "sh.kraftkit.version" has been set.  The value of this annotation is
-// discarded.
+// KraftKit.
 func IsOCIManifestKraftKitCompatible(manifest *ocispec.Manifest) (bool, error) {
 	if manifest == nil {
 		return false, fmt.Errorf("provided manifest is nil")
-	}
-
-	if manifest.Annotations == nil {
-		return false, fmt.Errorf("manifest does not contain any annotations")
-	}
-
-	if _, ok := manifest.Annotations[AnnotationKraftKitVersion]; !ok {
-		return false, fmt.Errorf("manifest does not contain '%s' annotation", AnnotationKraftKitVersion)
 	}
 
 	return true, nil
@@ -54,20 +34,10 @@ func IsOCIManifestKraftKitCompatible(manifest *ocispec.Manifest) (bool, error) {
 
 // IsOCIDescriptorKraftKitCompatible is a utility method that is used to
 // determine whether the provided OCI Specification Descriptor structure is
-// compatible with KraftKit.  Ultimately, this is achieved by testing whether
-// the annotation "sh.kraftkit.version" has been set.  The value of this
-// annotation is discarded.
+// compatible with KraftKit.
 func IsOCIDescriptorKraftKitCompatible(descriptor *ocispec.Descriptor) (bool, error) {
 	if descriptor == nil {
 		return false, fmt.Errorf("provided descriptor is nil")
-	}
-
-	if descriptor.Annotations == nil {
-		return false, fmt.Errorf("descriptor does not contain any annotations")
-	}
-
-	if _, ok := descriptor.Annotations[AnnotationKraftKitVersion]; !ok {
-		return false, fmt.Errorf("descriptor does not contain '%s' annotation", AnnotationKraftKitVersion)
 	}
 
 	return true, nil
